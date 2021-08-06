cfc0eab2e65de621de8357070ba24c9fb3de14a7
Michael Vandeberg <mvandeberg@users.noreply.github.com>
2021-08-05 17:18:00 -0700

    Only update on master #4

--- ci/after_success.sh
+++ ci/after_success.sh
@@ -2,46 +2,46 @@
 
 COMMIT_HASH=`git rev-parse --short HEAD`
 
-cd ~
+if [ "$TRAVIS_BRANCH" = "master" ] && [ "$TRAVIS_PULL_REQUEST" = "false" ]; then
+   cd ~
 
-git config --global user.email ${GITHUB_USER_EMAIL}
-git config --global user.name  ${GITHUB_USER_NAME}
+   git config --global user.email ${GITHUB_USER_EMAIL}
+   git config --global user.name  ${GITHUB_USER_NAME}
 
 
-#C++
-git clone https://${GITHUB_USER_TOKEN}@github.com/koinos/koinos-proto-cpp.git
+   #C++
+   git clone https://${GITHUB_USER_TOKEN}@github.com/koinos/koinos-proto-cpp.git
 
-pushd koinos-proto-cpp
+   pushd koinos-proto-cpp
 
-mkdir -p libraries/proto/include/koinos/proto
-find $TRAVIS_BUILD_DIR/build/cpp
-rsync -rvm --include "*.c++" --include "*/" --exclude "*" $TRAVIS_BUILD_DIR/build/cpp/ ./libraries/proto/
-rsync -rvm --include "*.h" --include "*/" --exclude "*" $TRAVIS_BUILD_DIR/build/cpp/ ./libraries/proto/include/koinos/proto/
+   mkdir -p libraries/proto/include/koinos/proto
+   rsync -rvm --include "*.c++" --include "*/" --exclude "*" $TRAVIS_BUILD_DIR/build/cpp/ ./libraries/proto/
+   rsync -rvm --include "*.h" --include "*/" --exclude "*" $TRAVIS_BUILD_DIR/build/cpp/ ./libraries/proto/include/koinos/proto/
 
-git add .
+   git add .
 
-if ! git diff --cached --quiet --exit-code; then
-   git commit -m "Update for koinos-proto commit $COMMIT_HASH"
-   git push https://${GITHUB_USER_TOKEN}@github.com/koinos/koinos-proto-cpp.git
+   if ! git diff --cached --quiet --exit-code; then
+      git commit -m "Update for koinos-proto commit $COMMIT_HASH"
+      git push https://${GITHUB_USER_TOKEN}@github.com/koinos/koinos-proto-cpp.git
+   fi
+
+   popd
+
+
+   #golang
+   git clone https://${GITHUB_USER_TOKEN}@github.com/koinos/koinos-proto-golang.git
+
+   pushd koinos-proto-golang
+
+   mkdir -p koinos
+   rsync -rvm $TRAVIS_BUILD_DIR/build/go/ ./koinos/
+
+   git add .
+
+   if ! git diff --cached --quiet --exit-code; then
+      git commit -m "Update for koinos-proto commit $COMMIT_HASH"
+      git push https://${GITHUB_USER_TOKEN}@github.com/koinos/koinos-proto-golang.git
+   fi
+
+   popd
 fi
-
-popd
-
-
-#golang
-git clone https://${GITHUB_USER_TOKEN}@github.com/koinos/koinos-proto-golang.git
-
-pushd koinos-proto-golang
-
-mkdir -p koinos
-find $TRAVIS_BUILD_DIR/build/go
-rsync -rvm $TRAVIS_BUILD_DIR/build/go/ ./koinos/
-
-git add .
-
-if ! git diff --cached --quiet --exit-code; then
-   git commit -m "Update for koinos-proto commit $COMMIT_HASH"
-   git push https://${GITHUB_USER_TOKEN}@github.com/koinos/koinos-proto-golang.git
-fi
-
-popd

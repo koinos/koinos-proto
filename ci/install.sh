598ca4c7ef3384e8eb8de1a59128131223b6580e
Michael Vandeberg <mvandeberg@users.noreply.github.com>
2021-10-12 09:45:58 -0700

    Get protoc-gen-doc

--- ci/install.sh
+++ ci/install.sh
@@ -7,6 +7,7 @@ wget https://github.com/protocolbuffers/protobuf/releases/download/v${PROTOBUF_V
 unzip protoc-${PROTOBUF_VERSION}-linux-x86_64.zip -d protobuf
 
 go install google.golang.org/protobuf/cmd/protoc-gen-go@latest
+go get -u github.com/pseudomuto/protoc-gen-doc/cmd/protoc-gen-doc@v1.5.0
 
 git clone --recursive https://github.com/koinos/EmbeddedProto
 pushd EmbeddedProto

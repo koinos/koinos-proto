e702a3c1d42618139bfc93f74eaa6bde22fcce25
Steve Gerbino <steve@gerbino.co>
2022-05-16 15:58:25 -0400

    Try using npm

--- ci/install.sh
+++ ci/install.sh
@@ -28,8 +28,7 @@ nvm usm $NODE_VERSION
 
 node --version
 
-yarn add lerna
-which lerna
+npm install --global lerna
 
 yarn add --dev protobufjs
 #yarn add --dev @roaminroe/as-proto-gen@$AS_PROTO_VERSION

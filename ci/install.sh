cc641949129b9c52b37e06f15bba23fe1017095b
Steve Gerbino <steve@gerbino.co>
2022-12-21 16:54:54 -0500

    #195: Initial exploratory impementation of OpenAPI v3 and gRPC

--- ci/install.sh
+++ ci/install.sh
@@ -8,6 +8,8 @@ source ~/.gimme/envs/go$GO_VERSION.env
 wget https://github.com/protocolbuffers/protobuf/releases/download/v${PROTOBUF_VERSION}/protoc-${PROTOBUF_VERSION}-linux-x86_64.zip
 unzip protoc-${PROTOBUF_VERSION}-linux-x86_64.zip -d protobuf
 
+go install github.com/google/gnostic/cmd/protoc-gen-openapi@latest
+
 go install google.golang.org/protobuf/cmd/protoc-gen-go@latest
 go get -u github.com/pseudomuto/protoc-gen-doc/cmd/protoc-gen-doc@v1.5.0
 
